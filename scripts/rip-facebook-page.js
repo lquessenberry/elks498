c9c455facd0808c0422c8efb6a011d32f963bfb7
lquessenberry <lquessenberry@gmail.com>
2026-05-12 16:26:05 -0500

    Scrape Facebook photos page - 57 photos from /photos URL

--- scripts/rip-facebook-page.js
+++ scripts/rip-facebook-page.js
@@ -7,7 +7,7 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const fbPageUrl = "https://www.facebook.com/jonesboro.elks";
+const fbPageUrl = "https://www.facebook.com/jonesboro.elks/photos";
 const outputDir = path.join(__dirname, "../public/images");
 const dataDir = path.join(__dirname, "../src/content/facebook");
 
